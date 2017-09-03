abed03881af2c3aed29caeb7e9075e728f0ace70
lukasz341 <lukasz341@tlen.pl>
2017-09-03 16:53:39 +0200

    usunięcie niepotrzebnej zmienniej

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@
 function drawTree(h){
 	for (var i=1; i<=h; i++){
 		var star='';
-		var spacja='';
 		for (var j=1; j<=i; j++){
 			star+= '*';
 		}
-		console.log(star);	
+		console.log(star);			
 	}
 }
 
